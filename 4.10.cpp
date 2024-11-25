d805e89c0d289f436a21b36122b587e133ea30d1
ngoc-dan <ngocdan191026@gmail.com>
2024-11-25 10:39:08 +0700

    Add files via upload

--- 4.10.cpp
+++ 4.10.cpp
@@ -4,28 +4,28 @@ int main(){
 	int a,b,c;
 	printf("nhap cac so a,b,c ");
 	scanf("%d""%d""%d" , &a,&b,&c);
-	if(a>b>c){
-		printf("thu tu tu nho den lon la : " ,c ,b ,a);
+	if(a>b&&b>c){
+		printf("thu tu tu nho den lon la : %d %d %d" ,c ,b ,a);
 	}
 	else{
-		if(b>c>a){
-			printf("thu tu tu nho den lon la : %d\n" ,a ,c ,b);
+		if(b>c && c>a){
+			printf("thu tu tu nho den lon la : %d %d %d" ,a , c , b);
 		}
 		else{
-			if(c>a>b){
-			printf("thu tu tu nho den lon la : %d\n" , b ,a ,c);
+			if(c>a&&a>b){
+			printf("thu tu tu nho den lon la : %d %d %d" , b ,a ,c);
 			}
 			else{
-				if(c>b>a){
-					printf("thu tu tu nho den lon la : %d\n" , a, b, c);
+				if(c>b&&b>a){
+					printf("thu tu tu nho den lon la : %d %d %d" , a, b, c);
 				}
 				else{
-					if(b>a>c){
-						printf("thu tu tu nho den lon la : %d\n" , c, a, b);
+					if(b>a&&a>c){
+						printf("thu tu tu nho den lon la : %d %d %d" , c, a, b);
 					}
 					else{
-						if(a>c>b){
-				    	printf("thu tu tu nho den lon la : %d\n" , b ,c ,a);
+						if(a>c&&c>b){
+				    	printf("thu tu tu nho den lon la : %d %d %d" , b ,c ,a);
 					}				    
 					}
 				}
